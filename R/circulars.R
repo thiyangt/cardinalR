bdb14a3851241e584719fcd49350440c4c28023f
JayaniLakshika <jayanilakshika76@gmail.com>
2025-03-27 20:27:39 +1100

    Updated function gen_three_cell_cycle

--- R/circulars.R
+++ R/circulars.R
@@ -1,12 +1,10 @@
-#' Generate Three Circular Clusters with Noise
+#' Generate Three Circular Clusters
 #'
-#' This function generates three circular clusters in 4D space with added noise dimensions.
+#' This function generates a dataset representing a structure with three circulars.
 #'
-#' @param n The total number of samples to generate.
-#' @param num_noise The number of additional noise dimensions to add to the data.
-#' @param min_n The minimum value for the noise dimensions.
-#' @param max_n The maximum value for the noise dimensions.
-#' @return A matrix containing the three circular clusters with added noise.
+#' @param n A numeric vector (default: c(200, 500, 300)) representing the sample sizes.
+#' @param p A numeric value (default: 4) representing the number of dimensions.
+#' @return A data containing the three circular clusters.
 #' @export
 #'
 #' @examples
@@ -16,7 +14,6 @@ gen_three_circulars <- function(n = c(200, 500, 300), p = 4) {
 
   if (p < 4) {
     stop(cli::cli_alert_danger("p should be 4 or greater."))
-
   }
 
   if (length(n) != 3) {
@@ -78,92 +75,80 @@ gen_three_circulars <- function(n = c(200, 500, 300), p = 4) {
   return(df)
 }
 
-#' Generate Cell Cycle Data with Noise
+#' Generate Three Cell Cycle Data
 #'
-#' This function generates a cell cycle dataset with added noise dimensions.
+#' This function generates a dataset representing a structure with three cell cycles.
 #'
-#' @param n The total number of samples to generate.
-#' @param num_noise The number of additional noise dimensions to add to the data.
-#' @param min_n The minimum value for the noise dimensions.
-#' @param max_n The maximum value for the noise dimensions.
-#' @return A matrix containing the cell cycle data with added noise.
+#' @param n A numeric vector (default: c(200, 500, 300)) representing the sample sizes.
+#' @param p A numeric value (default: 3) representing the number of dimensions.
+#' @return A data containing the three cell cycle data.
 #' @export
 #'
 #' @examples
 #' set.seed(20240412)
-#' cell_cycle_data <- cell_cycle(
-#'   n = 300, num_noise = 2, min_n = -0.05,
-#'   max_n = 0.05
-#' )
-cell_cycle <- function(n, num_noise, min_n, max_n) {
-  if (n <= 0) {
-    stop("Number of points should be a positive number.")
+#' cell_cycle_data <- gen_three_cell_cycle(n = c(200, 500, 300), p = 3)
+gen_three_cell_cycle <- function(n = c(200, 500, 300), p = 3) {
+
+  if (p < 3) {
+    stop(cli::cli_alert_danger("p should be 3 or greater."))
   }
 
-  if (num_noise < 0) {
-    stop("Number of noise dimensions should be a positive number.")
+  if (length(n) != 3) {
+    stop(cli::cli_alert_danger("n should contain exactly 3 values."))
   }
 
-  if (missing(n)) {
-    stop("Missing n.")
-  }
-
-  if (missing(num_noise)) {
-    stop("Missing num_noise.")
-  }
-
-  # To check that the assigned n is divided by three
-  if ((n %% 3) != 0) {
-    warning("The sample size should be a product of three.")
-    cluster_size <- floor(n / 3)
-  } else {
-    cluster_size <- n / 3
+  if (any(n < 0)) {
+    stop(cli::cli_alert_danger("Values in n should be positive."))
   }
 
 
   r1 <- 2
   r2 <- 1
 
-  theta <- stats::runif(cluster_size, 0, 2 * pi)
-  x <- rep(0, cluster_size)
-  y <- r1 * cos(theta)
-  z <- r2 * sin(theta)
+  theta1 <- stats::runif(n[1], 0, 2 * pi)
+  x1 <- rep(0, n[1])
+  x2 <- r1 * cos(theta1)
+  x3 <- r2 * sin(theta1)
 
-  df1 <- matrix(c(x, y, z), ncol = 3)
+  df1 <- tibble::tibble(x1 = x1,
+                        x2 = x2,
+                        x3 = x3)
 
-  x <- r2 * cos(theta)
-  y <- rep(0, cluster_size)
-  z <- r1 * sin(theta)
+  theta2 <- stats::runif(n[2], 0, 2 * pi)
+  x1 <- r2 * cos(theta2)
+  x2 <- rep(0, n[2])
+  x3 <- r1 * sin(theta2)
 
-  df2 <- matrix(c(x, y, z), ncol = 3)
+  df2 <- tibble::tibble(x1 = x1,
+                        x2 = x2,
+                        x3 = x3)
 
-  x <- r1 * cos(theta)
-  y <- r2 * sin(theta)
-  z <- rep(0, cluster_size)
+  theta3 <- stats::runif(n[3], 0, 2 * pi)
+  x1 <- r1 * cos(theta3)
+  x2 <- r2 * sin(theta3)
+  x3 <- rep(0, n[3])
 
-  df3 <- matrix(c(x, y, z), ncol = 3)
+  df3 <- tibble::tibble(x1 = x1,
+                        x2 = x2,
+                        x3 = x3)
 
-  df <- rbind(df1, df2, df3)
+  df <- dplyr::bind_rows(df1, df2, df3)
 
-  if (num_noise != 0) {
-    if (missing(min_n)) {
-      stop("Missing min_n.")
-    }
+  if (p > 3) {
 
-    if (missing(max_n)) {
-      stop("Missing max_n.")
-    }
+    cli::cli_alert_info("Adding noise dimensions to reach the desired dimensionality.")
 
     noise_mat <- gen_noise_dims(
-      n = dim(df)[1], num_noise = num_noise,
-      min_n = min_n, max_n = max_n
+      n = NROW(df), num_noise = p - 3,
+      min_n = -0.5, max_n = 0.5
     )
-    df <- cbind(df, noise_mat)
+    colnames(noise_mat) <- paste0("x", 4:p)
+    df <- dplyr::bind_cols(df, noise_mat)
 
-    df
-  } else {
-    df
   }
+
+  cli::cli_alert_success("Data generation completed successfully! 🎉")
+  return(df)
 }
 
 #' Generate Curvy Cell Cycle Data with Noise
