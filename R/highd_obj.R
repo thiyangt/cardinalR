e44ce4e19daa93cbc15850a62b23a01d5cd939ba
JayaniLakshika <jayanilakshika76@gmail.com>
2025-03-31 17:14:09 +1100

    Updated highd_obj.R

--- R/highd_obj.R
+++ R/highd_obj.R
@@ -104,107 +104,106 @@ gen_intersect_3d_two_conic_spiral <- function(n = c(500, 300), p = 3) {
 
 }
 
-#' Generate points on a Dini's surface.
+#' Generate intersected tow dini surface shapes
 #'
-#' This function generates points on a Dini's surface.
+#' This function generates intersected tow dini surface shapes.
 #'
-#' @param a Outer radius of the surface.
-#' @param b Space between loops.
+#' @param n A numeric vector (default: c(500, 300)) representing the sample sizes.
+#' @param p A numeric value (default: 3) representing the number of dimensions.
 #'
-#' @return A matrix containing the generated points on the surface.
-#'
-#' @examples
-#' set.seed(20240412)
-#' dini_surface_3d_row(a = 1, b = 1)
-#'
-#' @export
-dini_surface_3d_row <- function(a = 1, b = 1) {
-  u <- stats::runif(1, min = 0, max = 4 * pi)
-  v <- stats::runif(1, min = 0.0000000001, max = 2)
-  x <- a * cos(u) * sin(v)
-  y <- a * sin(u) * sin(v)
-  z <- a * (cos(v) + log(tan(v / 2))) + (b * u)
-  return(cbind(x, y, z))
-}
-
-#' Generate points sampled from the Dini surface with optional noise.
-#'
-#' This function generates points sampled from the Dini surface along with optional noise.
-#'
-#' @param n Total number of data points to generate.
-#' @param num_noise Number of additional noise dimensions to add to the data.
-#' @param min_n Minimum value for the noise added to the data.
-#' @param max_n Maximum value for the noise added to the data.
-#'
-#' @return A matrix containing the generated data points with or without added noise.
-#' @importFrom purrr reduce
+#' @return A data containing the two intersected dini surface shapes.
 #'
 #' @examples
 #' set.seed(20240412)
-#' dini_surface_3d(n = 100, num_noise = 2, min_n = -0.05, max_n = 0.05)
-#'
-#' @export
-dini_surface_3d <- function(n, num_noise, min_n, max_n) {
-  if (n <= 0) {
-    stop("Number of points should be a positive number.")
+#' gen_intersect_3d_two_dini_surface(n = c(500, 300), p = 3)
+gen_intersect_3d_two_dini_surface <- function(n = c(500, 300), p = 3) {
+
+  if (p < 3) {
+    stop(cli::cli_alert_danger("p should be 3 or greater."))
   }
 
-  if (num_noise < 0) {
-    stop("Number of noise dimensions should be a positive number.")
+  if (length(n) != 2) {
+    stop(cli::cli_alert_danger("n should contain exactly 2 values."))
   }
 
-  if (missing(n)) {
-    stop("Missing n.")
+  if (any(n < 0)) {
+    stop(cli::cli_alert_danger("Values in n should be positive."))
   }
 
-  if (missing(num_noise)) {
-    stop("Missing num_noise.")
-  }
+  df1 <- tibble::as_tibble(geozoo::dini.surface(n = n[1])$points, .name_repair = "unique") |>
+    set_names(paste0("x", 1:3))
+  df2 <- tibble::as_tibble(geozoo::dini.surface(n = n[2])$points[,c(2, 1, 3)], .name_repair = "unique") |>
+    set_names(paste0("x", 1:3))
 
-  df_list <- lapply(1:n, function(i) dini_surface_3d_row(a = 1, b = 1))
-  df <- purrr::reduce(df_list, rbind)
+  df <- dplyr::bind_rows(df1, df2)
 
-  if (num_noise != 0) {
-    if (missing(min_n)) {
-      stop("Missing min_n.")
-    }
+  if (p > 3) {
 
-    if (missing(max_n)) {
-      stop("Missing max_n.")
-    }
+    cli::cli_alert_info("Adding noise dimensions to reach the desired dimensionality.")
 
     noise_mat <- gen_noise_dims(
-      n = dim(df)[1], num_noise = num_noise,
-      min_n = min_n, max_n = max_n
+      n = NROW(df), num_noise = p - 3,
+      min_n = -0.5, max_n = 0.5
     )
-    df <- cbind(df, noise_mat)
+    colnames(noise_mat) <- paste0("x", 4:p)
+    df <- dplyr::bind_cols(df, noise_mat)
 
-    df
-  } else {
-    df
   }
+
+  cli::cli_alert_success("Data generation completed successfully! 🎉")
+  return(df)
+
 }
 
-#' Generate points on a Roman surface in 3D space.
+#' Generate overlapped tow dini surface shapes
 #'
-#' This function generates points on a Roman surface in 3D space.
+#' This function generates overlapped tow dini surface shapes.
 #'
-#' @param a Maximum radius of the object.
+#' @param n A numeric vector (default: c(500, 300)) representing the sample sizes.
+#' @param p A numeric value (default: 3) representing the number of dimensions.
 #'
-#' @return A matrix containing the generated points on the Roman surface in 3D space.
+#' @return A data containing the two overlapped dini surface shapes.
 #'
 #' @examples
 #' set.seed(20240412)
-#' roman_surface_3d_row(a = 1)
-#'
-#' @export
-roman_surface_3d_row <- function(a = 1) {
-  u <- stats::runif(1, min = 0, max = pi)
-  v <- stats::runif(1, min = 0, max = pi)
-  x <- a^2 * cos(v) * cos(v) * sin(2 * u) / 2
-  y <- a^2 * sin(u) * sin(2 * v) / 2
-  z <- a^2 * cos(u) * sin(2 * v) / 2
-  return(cbind(x, y, z))
+#' gen_overlap_3d_two_dini_surface(n = c(500, 300), p = 3)
+gen_overlap_3d_two_dini_surface <- function(n = c(500, 300), p = 3) {
+
+  if (p < 3) {
+    stop(cli::cli_alert_danger("p should be 3 or greater."))
+  }
+
+  if (length(n) != 2) {
+    stop(cli::cli_alert_danger("n should contain exactly 2 values."))
+  }
+
+  if (any(n < 0)) {
+    stop(cli::cli_alert_danger("Values in n should be positive."))
+  }
+
+  df1 <- tibble::as_tibble(geozoo::dini.surface(n = n[1])$points, .name_repair = "unique") |>
+    set_names(paste0("x", 1:3))
+  df2 <- tibble::as_tibble(geozoo::dini.surface(n = n[2])$points[,c(3, 2, 1)], .name_repair = "unique") |>
+    set_names(paste0("x", 1:3))
+
+  df <- dplyr::bind_rows(df1, df2)
+
+  if (p > 3) {
+
+    cli::cli_alert_info("Adding noise dimensions to reach the desired dimensionality.")
+
+    noise_mat <- gen_noise_dims(
+      n = NROW(df), num_noise = p - 3,
+      min_n = -0.5, max_n = 0.5
+    )
+    colnames(noise_mat) <- paste0("x", 4:p)
+    df <- dplyr::bind_cols(df, noise_mat)
+
+  }
+
+  cli::cli_alert_success("Data generation completed successfully! 🎉")
+  return(df)
+
 }
 
 #' Generate data points on a Roman surface with optional noise.
