d7c5a766e300272ac9ffbb41aef17d265b3f711e
JayaniLakshika <jayanilakshika76@gmail.com>
2025-04-07 14:16:46 +1000

    Added triangular prism clusters data

--- R/triangular.R
+++ R/triangular.R
@@ -1,15 +1,15 @@
-#' Generate 3D Triangular Prism
+#' Generate p-D Triangular Prism
 #'
-#' This function generates 3D triangular prism  datasets.
+#' This function generates p-D triangular prism datasets.
 #'
-#' @param n A numeric value (default: 300) representing the sample size.
-#' @param p A numeric value (default: 3) representing the number of dimensions.
-#' @return A data containing a triangular prism in 3D.
+#' @param n A numeric value (default: 500) representing the sample size.
+#' @param p A numeric value (default: 4) representing the number of dimensions.
+#' @return A data containing a triangular prism in p-D.
 #' @export
 #'
 #' @examples
 #' set.seed(20240412)
-#' triangular_3d_data <- gen_tri_prism(n = 300, p = 3)
+#' triangular_data <- gen_tri_prism(n = 300, p = 3)
 gen_tri_prism <- function(n = 500, p = 4) {
 
   if (p < 2) {
@@ -42,62 +42,68 @@ gen_tri_prism <- function(n = 500, p = 4) {
 }
 
 
-#' Generate Triangular Plane with Background Noise
+#' Generate Triangular Prism clusters
 #'
-#' This function generates a triangular plane dataset with background noise dimensions.
+#' This function generates p-D triangular prism clusters datasets.
 #'
-#' @param n The total number of samples to generate.
-#' @param num_noise The number of additional noise dimensions to add to the data.
-#' @param min_n The minimum value for the noise dimensions.
-#' @param max_n The maximum value for the noise dimensions.
-#' @return A matrix containing the triangular plane dataset with background noise.
+#' @param n A numeric vector (default: c(200, 500, 300)) representing the sample sizes.
+#' @param p A numeric value (default: 4) representing the number of dimensions.
+#' @param k A numeric value (default: 3) representing the number of clusters.
+#' @return A data containing a triangular prism clusters.
 #' @export
 #'
 #' @examples
 #' set.seed(20240412)
-#' triangular_plane_data <- gen_tri_plane_with_bkg(n = 300, p = 3)
-gen_tri_plane_with_bkg <- function(n = 300, p = 3) {
+#' triangular_clusts_data <- gen_clusts_tri(n = c(200, 300, 500), p = 4, k = 3)
+gen_clusts_tri <- function(n = c(200, 300, 500), p = 4, k = 3) {
 
-  if (p < 3) {
-    stop(cli::cli_alert_danger("p should be 3 or greater."))
+  if (k < 2) {
+    cli::cli_abort("k should be greater than 2.")
+  }
+
+  if (p < 2) {
+    cli::cli_abort("p should be greater than 2.")
+  }
+
+  if (length(n) != k) {
+    cli::cli_abort("n should contain exactly {.val {k}} values.")
   }
 
   if (any(n < 0)) {
-    stop(cli::cli_alert_danger("Values in n should be positive."))
+    cli::cli_abort("Values in n should be positive.")
   }
 
-  trace_point <- stats::runif(2)
-  corner_points <- geozoo::simplex(p=2)$points
+  ## To generate the shifting vector
+  shift_vec <- stats::runif(k, -1, 1)
 
-  df1 <- tibble::tibble(
-    x1 = rep(0, n),
-    x2 = rep(0, n)
-  )
+  df <- tibble::tibble()
 
-  for (i in 1:(n * 0.7)) {
-    trace_point <- (corner_points[sample(3, 1), ] + trace_point) / 2
-    df1[i, ] <- as.list(trace_point)
+  for (j in 1:k) {
+
+    trace_point <- stats::runif(p)
+    corner_points <- geozoo::simplex(p=p)$points
+
+    data_list <- lapply(1:p, function(i) rep(0, n[j]))
+    names(data_list) <- paste0("x", 1:p)
+
+    df1 <- tibble::tibble(!!!data_list)
+
+    for (i in 1:n[j]) {
+      trace_point <- (corner_points[sample((p + 1), 1), ] + trace_point) / 2
+      df1[i, ] <- as.list(trace_point)
+    }
+
+    df1 <- shift_vec[j] * df1
+
+    df1 <- df1 |>
+      dplyr::mutate(cluster = paste0("cluster", j))
+
+    df <- dplyr::bind_rows(df, df1)
+
   }
 
-  if (p > 2) {
-
-    cli::cli_alert_info("Adding noise dimensions to reach the desired dimensionality.")
-
-    noise_mat <- gen_noise_dims(
-      n = NROW(df1), num_noise = p - 2,
-      min_n = -0.5, max_n = 0.5
-    )
-    colnames(noise_mat) <- paste0("x", 3:p)
-    df1 <- dplyr::bind_cols(df1, noise_mat)
-
-  }
-
-  df2 <- gen_bkg_noise(
-    n = n * 0.3, num_dims = NCOL(df1), mean = 0.025,
-    sd = 0.5
-  )
-
-  df <- dplyr::bind_rows(df1, df2, -df1)
+  ## To swap rows
+  df <- randomize_rows(df)
 
   cli::cli_alert_success("Data generation completed successfully! 🎉")
   return(df)
