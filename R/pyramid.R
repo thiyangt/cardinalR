2e7af6f16c137d467358b0341c2f13081c77e6f5
JayaniLakshika <jayanilakshika76@gmail.com>
2025-04-08 17:22:19 +1000

    Added triangular one to pyramid

--- R/pyramid.R
+++ R/pyramid.R
@@ -1,3 +1,48 @@
+#' Generate p-D Triangular Pyramid With Pyramid shaped Hole
+#'
+#' This function generates p-D triangular pyramid datasets.
+#'
+#' @param n A numeric value (default: 500) representing the sample size.
+#' @param p A numeric value (default: 4) representing the number of dimensions.
+#' @return A data containing a triangular pyramid in p-D with a hole.
+#' @export
+#'
+#' @examples
+#' set.seed(20240412)
+#' triangular_data <- gen_triprism(n = 300, p = 3)
+gen_pyrhole <- function(n = 500, p = 4) {
+
+  if (p < 2) {
+    cli::cli_abort("p should be greater than 2.")
+  }
+
+  if (length(n) != 1) {
+    cli::cli_abort("n should be a single integer specifying the number of points.")
+  }
+
+  if (n < 0) {
+    cli::cli_abort("n should be positive.")
+  }
+
+  trace_point <- stats::runif(p)
+  corner_points <- geozoo::simplex(p=p)$points
+
+  data_list <- lapply(1:p, function(i) rep(0, n))
+  names(data_list) <- paste0("x", 1:p)
+
+  df <- tibble::tibble(!!!data_list)
+
+  for (i in 1:n) {
+    trace_point <- (corner_points[sample((p + 1), 1), ] + trace_point) / 2
+    df[i, ] <- as.list(trace_point)
+  }
+
+  cli::cli_alert_success("Data generation completed successfully! 🎉")
+  return(df)
+}
+
+
+
 #' Generate Rectangular Based Pyramid
 #'
 #' This function generates a dataset representing a rectangular based pyramid.
