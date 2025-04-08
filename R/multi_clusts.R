5e1a177d34e8e3aa25c0ea0d99a6d30992ae00ff
JayaniLakshika <jayanilakshika76@gmail.com>
2025-04-08 16:38:13 +1000

    Initial setup for gen_multicluster

---
+++ R/multi_clusts.R
@@ -0,0 +1,26 @@
+gen_multicluster <- function(n, p, k, location, spread, shape_vec, bkg_param) {
+
+  dfs <- list()
+
+  ## To generate different shaped clusters
+  if (shape_vec %in% "gen_gaussian") {
+
+    dfs[[1]] <- gen_gaussian() |>
+      dplyr::mutate(cluster = paste0("cluster", "1"))
+
+  }
+
+  ## To combine the data
+  df <- tibble::as_tibble(dfs)
+
+  ## Add background noise
+  noise_df <- gen_bkg_noise() |>
+    dplyr::mutate(cluster = paste0("bkg_noise"))
+  df <- dplyr::bind_rows(df, noise_df)
+
+  ## Swap rows
+  df <- randomize_rows(df)
+
+  return(df)
+
+}
