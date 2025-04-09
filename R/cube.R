743d644f5b340aeb9bf77e90b7f4294aabccee69
JayaniLakshika <jayanilakshika76@gmail.com>
2025-04-09 10:41:04 +1000

    Added gen_unifcube

--- R/cube.R
+++ R/cube.R
@@ -34,20 +34,29 @@ gen_gridcube <- function(n = 625, p = 4) {
 
 }
 
-
-gen_unifcube <- function(n = c(10, 10, 10, 10), p = 4) {
-
-  if (length(n) != p) {
-    stop(cli::cli_alert_danger("n should contain exactly p values."))
-  }
+#' Generate Cube with uniform points
+#'
+#' This function generates a grid dataset with specified uniform points along each axes..
+#'
+#' @param n A numeric vector (default: c(10, 10)) representing the number of grid points along each axes.
+#' @param p A numeric value (default: 4) representing the number of dimensions.
+#' @return A data containing the cube with uniform points.
+#' @export
+#'
+#' @examples
+#' set.seed(20240412)
+#' unif_cube <- gen_unifcube(n = 625, p = 4)
+gen_unifcube <- function(n = 625, p = 4) {
 
   if (any(n < 0)) {
     stop(cli::cli_alert_danger("Values in n should be positive."))
   }
 
-  df <- tidyr::expand_grid(!!!purrr::map(p, seq_len))
+  df <- matrix(runif(n * p, min = -0.5, max = 0.5),
+                   nrow = n, ncol = p)
+  df <- tibble::as_tibble(df)
 
-  names(df) <- paste0("x", 1:NCOL(df))
+  names(df) <- paste0("x", 1:p)
 
   cli::cli_alert_success("Data generation completed successfully! 🎉")
   return(df)
