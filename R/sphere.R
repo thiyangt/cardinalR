54725570fd22546d29f442908387f34b1327ec48
JayaniLakshika <jayanilakshika76@gmail.com>
2025-04-13 18:53:49 +1000

    Updated gen_curvycycle

--- R/sphere.R
+++ R/sphere.R
@@ -54,14 +54,13 @@ gen_circle <- function(n = 500, p = 3){
 #'
 #' @param n A numeric value (default: 500) representing the sample size.
 #' @param p A numeric value (default: 4) representing the number of dimensions.
-#' @param r A numeric value (default: sqrt(3) / 3) representing the radius factor along x2.
 #' @return A data containing a curvy cell cycle.
 #' @export
 #'
 #' @examples
 #' set.seed(20240412)
-#' curvy_cycle_data <- gen_curvycycle(n = 500, p = 4, shift = c(0, sqrt(3) / 3, 0), scale_fac = c(1, 1, 1/3))
-gen_curvycycle <- function(n = 500, p = 4, shift = c(0, sqrt(3) / 3, 0), scale_fac = c(1, 1, 1/3)){
+#' curvy_cycle_data <- gen_curvycycle(n = 500, p = 4)
+gen_curvycycle <- function(n = 500, p = 4){
 
   if (p <= 3) {
     cli::cli_abort("p should be greater than 3.")
@@ -77,9 +76,9 @@ gen_curvycycle <- function(n = 500, p = 4, shift = c(0, sqrt(3) / 3, 0), scale_f
 
   theta <- stats::runif(n, 0.0, 2 * pi)
   coords <- matrix(0, nrow = n, ncol = p)
-  coords[, 1] <- scale_fac[1] * (shift[1] + cos(theta))
-  coords[, 2] <- scale_fac[2] * (shift[2] + sin(theta))
-  coords[, 3] <- scale_fac[3] * (shift[3] + cos(3 * theta))
+  coords[, 1] <- cos(theta)
+  coords[, 2] <- sqrt(3) / 3 + sin(theta)
+  coords[, 3] <- 1/3 * cos(3 * theta)
 
   # Introduce scaling factors for subsequent dimensions
   scaling_factors <- sqrt(cumprod(c(1, rep(0.5, p - 3)))) # Example: decreasing scale
