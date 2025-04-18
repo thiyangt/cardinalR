84155699119d3ff1cd821df7ed3086d5859af951
JayaniLakshika <jayanilakshika76@gmail.com>
2025-04-18 11:42:36 +1000

    Moved function to cube.R

--- R/cube.R
+++ R/cube.R
@@ -81,3 +81,41 @@ gen_unifcube <- function(n = 500, p = 4) {
 
 
 }
+
+#' Generate Cube with Hole
+#'
+#' This function generates a dataset representing a cube with a hole.
+#'
+#' @param n A numeric value (default: 500) representing the sample size.
+#' @param p A numeric value (default: 4) representing the number of dimensions.
+#'
+#' @return A data containing the cube data with a hole.
+#' @export
+#'
+#' @examples
+#' set.seed(20240412)
+#' cube_hole_data <- gen_cubehole(n = 1000, p = 4)
+gen_cubehole <- function(n = 500, p = 4) {
+
+  if (p < 2) {
+    cli::cli_abort("p should be greater than 2.")
+  }
+
+  if (n < 0) {
+    cli::cli_abort("n should be positive.")
+  }
+
+  plane_points <- gen_unifcube(n = n, p = p)
+
+  # Compute Euclidean distance from the center
+  distances <- sqrt(rowSums(plane_points^2))
+
+  # Remove points inside the hole
+  hole_radius <- 0.5
+  plane_points <- plane_points |>
+    dplyr::filter(distances > hole_radius)
+
+  cli::cli_alert_success("Data generation completed successfully! 🎉")
+  return(plane_points)
+
+}
