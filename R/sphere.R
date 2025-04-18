abe2542ab70055ad535a95ec5865b8e78d41ad4a
JayaniLakshika <jayanilakshika76@gmail.com>
2025-04-18 10:57:14 +1000

    Starts working on gen_solidsphere

--- R/sphere.R
+++ R/sphere.R
@@ -112,22 +112,18 @@ gen_curvycycle <- function(n = 500, p = 4){
 #' set.seed(20240412)
 #' sphere_data <- gen_unifSphere(n = 500, p = 4)
 #' head(sphere_data, 5)
-gen_unifSphere <- function(n = 500, p = 4){
+gen_unitSphere <- function(n = 500, p = 4){
 
   if (p < 3) {
     cli::cli_abort("p should be greater than 3.")
-  }
-
-  if (r <= 0) {
-    cli::cli_abort("r should be positive.")
   }
 
   if (n <= 0) {
     cli::cli_abort("n should be positive.")
   }
 
-  u <- runif(n, -1, 1)                 # cos(phi)
-  theta <- runif(n, 0, 2 * pi)        # azimuth
+  u <- stats::runif(n, -1, 1)                 # cos(phi)
+  theta <- stats::runif(n, 0, 2 * pi)        # azimuth
 
   x1 <- sqrt(1 - u^2) * cos(theta)
   x2 <- sqrt(1 - u^2) * sin(theta)
@@ -149,6 +145,23 @@ gen_unifSphere <- function(n = 500, p = 4){
   cli::cli_alert_success("Data generation completed successfully! 🎉")
   return(df)
 
+}
+
+gen_solidsphere <- function(num_points, radius = 1.0) {
+  points <- matrix(NA, nrow = num_points, ncol = 3)
+  for (i in 1:num_points) {
+    # Generate random spherical coordinates
+    r <- radius * runif(1)^(1/3) # Radius between 0 and sphere radius, cubed root for uniform volume
+    theta <- runif(1, 0, 2 * pi)     # Azimuth angle [0, 2*pi)
+    phi <- acos(runif(1, -1, 1))    # Polar angle [0, pi]
+
+    # Convert to Cartesian coordinates
+    x <- r * sin(phi) * cos(theta)
+    y <- r * sin(phi) * sin(theta)
+    z <- r * cos(phi)
+    points[i, ] <- c(x, y, z)
+  }
+  return(points)
 }
 
 #' Generate Grided Sphere
